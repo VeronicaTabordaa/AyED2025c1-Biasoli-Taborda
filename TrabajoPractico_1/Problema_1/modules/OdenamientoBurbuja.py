939253cbcd1222987d309b1f67c989ce03b9bb73
VeronicaTabordaa <veronica.taborda@ingenieria.uner.edu.ar>
2025-04-11 13:27:23 -0300

    arreglar graficador de tiempos

--- TrabajoPractico_1/Problema_1/modules/OdenamientoBurbuja.py
+++ TrabajoPractico_1/Problema_1/modules/OdenamientoBurbuja.py
@@ -2,6 +2,7 @@
 # Crear tantos módulos como sea necesario para organizar el código
 import random
 
+
 def OrdenamientoBurbuja(lista1):
     for extremo in range(len(lista1)-1,0,-1):
         for i in range(extremo):
@@ -17,19 +18,61 @@ if __name__=="__main__":
     listaordenadaburb = OrdenamientoBurbuja(lista1)
     #print ("lista ordenada:",listaordenadaburb)
 
+contador=0
+ordenada = True
+while contador < len(listaordenadaburb) - 1:
+    if listaordenadaburb[contador] > listaordenadaburb[contador + 1]:  
+        ordenada = False
+        break  
+    contador += 1
+if ordenada:
+    print("La lista está correctamente ordenada.")
+else:
+    print("La lista NO está ordenada.")
 
-    contador=0
-    ordenada = True
-    while contador < len(listaordenadaburb) - 1:
-        if listaordenadaburb[contador] > listaordenadaburb[contador + 1]:  
-            ordenada = False
-            break  
-        contador += 1
-   
-    if ordenada:
-        print("La lista está correctamente ordenada.")
-    else:
-        print("La lista NO está ordenada.")
+import time 
+import matplotlib.pyplot as plt
+#creo funciones para medir tiempo y para graficar
+
+def medir_tiempo(funcion, lista):
+    inicio=time.time()
+    funcion(lista)
+    fin=time.time()
+    return fin-inicio
+
+def graficar_tiempos(tiempos, tamanos):
+    plt.plot(tamanos, tiempos, marker='o')
+    plt.title('Tiempo de ejecución del algoritmo de ordenamiento por burbuja')
+    plt.xlabel('Tamaño de la lista')
+    plt.ylabel('Tiempo (segundos)')
+    plt.grid(True)
+    plt.show()
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 '''    def ordenada_si_o_no(lista):
