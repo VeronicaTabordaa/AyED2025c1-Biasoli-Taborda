d1269c4c03869d3e102560f65072ec0d708ed2bf
VeronicaTabordaa <veronica.taborda@ingenieria.uner.edu.ar>
2025-04-15 17:23:02 -0300

    creacion modulo ordenamiento por residuos

--- TrabajoPractico_1/Problema_1/modules/OdenamientoBurbuja.py
+++ TrabajoPractico_1/Problema_1/modules/OdenamientoBurbuja.py
@@ -1,6 +1,8 @@
 # módulo para organizar funciones o clases utilizadas en nuestro proyecto
-# Crear tantos módulos como sea necesario para organizar el código
-import random
+# # Crear tantos módulos como sea necesario para organizar el código
+import random 
+import matplotlib.pyplot as plt
+import time
 
 
 def OrdenamientoBurbuja(lista1):
@@ -16,7 +18,7 @@ if __name__=="__main__":
     lista1 = [random.randint(10000,99999) for _ in range(500)]
     #print("Lista 1:", lista1)
     listaordenadaburb = OrdenamientoBurbuja(lista1)
-    #print ("lista ordenada:",listaordenadaburb)
+    print ("lista ordenada:",listaordenadaburb)
 
 contador=0
 ordenada = True
@@ -30,24 +32,6 @@ if ordenada:
 else:
     print("La lista NO está ordenada.")
 
-import time 
-import matplotlib.pyplot as plt
-#creo funciones para medir tiempo y para graficar
-
-def medir_tiempo(funcion, lista):
-    inicio=time.time()
-    funcion(lista)
-    fin=time.time()
-    return fin-inicio
-
-def graficar_tiempos(tiempos, tamanos):
-    plt.plot(tamanos, tiempos, marker='o')
-    plt.title('Tiempo de ejecución del algoritmo de ordenamiento por burbuja')
-    plt.xlabel('Tamaño de la lista')
-    plt.ylabel('Tiempo (segundos)')
-    plt.grid(True)
-    plt.show()
-    
 
 
 
@@ -74,14 +58,3 @@ def graficar_tiempos(tiempos, tamanos):
 
 
 
-
-'''    def ordenada_si_o_no(lista):
-        for i in range(len(listaordenadaburb)-1):
-            if listaordenadaburb[i]>listaordenadaburb[i+1]:
-                return False
-        return True
-    
-    if ordenada_si_o_no(listaordenadaburb)==True:
-        print("está ordenada")
-    else:
-        print("no está ordenada") '''
