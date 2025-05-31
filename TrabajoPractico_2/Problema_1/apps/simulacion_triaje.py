c520a5e958268b7a60e0d6d57e4bb644f7740e09
VeronicaTabordaa <veronica.taborda@ingenieria.uner.edu.ar>
2025-05-31 20:46:47 -0300

    Problema 1 resuelto

--- TrabajoPractico_2/Problema_1/apps/simulacion_triaje.py
+++ TrabajoPractico_2/Problema_1/apps/simulacion_triaje.py
@@ -13,7 +13,22 @@ def Simular_paciente():
     PQ.push(Paciente("Juan", 2), 2)
     PQ.push(Paciente("Ana", 1), 1)
     PQ.push(Paciente("Luis", 3), 3)
+    PQ.push(Paciente("Maria", 2), 2)
+    PQ.push(Paciente("Sofía", 1), 1)
+    PQ.push(Paciente("Pedro", 1), 1) 
+    PQ.push(Paciente("Mateo", 3), 3)
+    PQ.push(Paciente("Valentina", 2), 2)
+    PQ.push(Paciente("Diego", 1), 1)
+    PQ.push(Paciente("Lucía", 3), 3)
+    PQ.push(Paciente("Santiago", 2), 2)
+    PQ.push(Paciente("Camila", 1), 1)
+    PQ.push(Paciente("Nicolás", 3), 3)
+    PQ.push(Paciente("Julieta", 2), 2)
+    PQ.push(Paciente("Benjamín", 1), 1)   
 
     while not PQ.is_empty():
         paciente = PQ.pop()
         print("Atendiendo a:", paciente)
+
+if __name__ == "__main__":
+    Simular_paciente()
--- TrabajoPractico_2/Problema_1/tests/test_perioridad.py
+++ TrabajoPractico_2/Problema_1/tests/test_perioridad.py
@@ -4,8 +4,8 @@
 import unittest
 from modules.Prioridad_Queue import PrioridadQueue
 
-class TestPriorityQueue(unittest.TestCase):
-    def test_order(self):
+class TestPrioridadQueue(unittest.TestCase):
+    def test_orden(self):
         pq = PrioridadQueue()
         pq.push("A", 2)
         pq.push("B", 1)
@@ -14,6 +14,17 @@ class TestPriorityQueue(unittest.TestCase):
         self.assertEqual(pq.pop(), "B")
         self.assertEqual(pq.pop(), "A")
         self.assertEqual(pq.pop(), "C")
+
+    def test_orden_de_llegada(self):
+        pq = PrioridadQueue()
+        pq.push("A", 1)
+        pq.push("B", 1)
+        pq.push("C", 1)
+
+        self.assertEqual(pq.pop(), "A")
+        self.assertEqual(pq.pop(), "B")
+        self.assertEqual(pq.pop(), "C")
+
 
 if __name__ == '__main__':
     unittest.main()
