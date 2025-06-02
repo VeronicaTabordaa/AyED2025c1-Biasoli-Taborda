6a6b46c39bf11cb1afd6a243e94b3c8e6cbbb9e1
VeronicaTabordaa <veronica.taborda@ingenieria.uner.edu.ar>
2025-06-02 09:05:54 -0300

    creo carpeta Problema2

---
+++ TrabajoPractico_2/Problema_2/modules/modulo1.py
@@ -0,0 +1,5 @@
+# módulo para organizar funciones o clases utilizadas en nuestro proyecto
+# Crear tantos módulos como sea necesario para organizar el código
+
+
+Escribo algo acá como para que se guarde
